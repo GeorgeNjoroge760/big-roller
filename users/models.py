d249a310378084658faf0b10544ee840e9cba05c
George Njoroge <georgenjoroge8038@gmail.com>
2026-06-17 09:52:30 +0300

    Fix UserProfile signal to set admin role for superusers

--- users/models.py
+++ users/models.py
@@ -23,7 +23,8 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        role = 'admin' if instance.is_superuser else 'waiter'
+        UserProfile.objects.create(user=instance, role=role)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
